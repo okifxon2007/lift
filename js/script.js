b3a7e34cfc69789c0db045ef4ae3d01e93cf3710
okifxon <shokirjonovokifxon46@gmail.com>
2024-11-14 17:40:24 +0300

    vazifa

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let btn2 = document.querySelector(".btn2")
 let btn3 = document.querySelector(".btn3")
 let btn4 = document.querySelector(".btn4")
 let btn5 = document.querySelector(".btn5")
+let btn6 = document.querySelector(".btn6")
+let btn7 = document.querySelector(".btn7")
 let lift = document.querySelector(".lift")
 let eshik = document.querySelector(".eshik")
 let sound = new Audio('sound/elvator.mp3');
@@ -39,6 +41,18 @@ btn5.addEventListener('click', function() {
     qavatname.innerHTML = '5Qavat'
     lift.style.marginTop = '-0px'
     lift.style.transition = '1s all ease'
-     eshik.style.backgroundColor = 'rgb(85, 83, 83)'
+     eshik.style.backgroundColor = '#303030'
     sound.play()
+});
+btn6.addEventListener('click', function() {
+    qavatname.innerHTML = 'Lift ochildi'
+    lift.style.transition = '1s all ease'
+     eshik.style.backgroundColor = '#303030'
+     eshik.style.width = '0px'
+});
+btn7.addEventListener('click', function() {
+    qavatname.innerHTML = 'Lift yopildi'
+    lift.style.transition = '1s all ease'
+    eshik.style.backgroundColor = 'white'
+    eshik.style.width = '70px'
 });
